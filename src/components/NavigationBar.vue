5f10e317589de23f67fd60991706119e00ac7ad6
haihan yin <haihan.yin@atos.net>
2017-11-09 12:39:01 +0100

    logout is only implemented at frontend, need improvements at backend

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -4,41 +4,43 @@
       <div class="navbar-header">
         <router-link :to="'/welcome'" class="navbar-brand">FindIt-Vue</router-link>
       </div>
-      <template v-if="this.$store.state.isLoggedOn && !this.$store.state.userInfo.userRoles.includes('ADMIN')">
-        <ul class="nav navbar-nav">
-          <li class="dropdown" v-bind:class="{active: openItem}">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-              <span class="glyphicon glyphicon-plus"></span> Add Item
-              <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li>
-                <router-link :to="'/openDigitalItem'">
-                  <span @click="clickOpenDigitalItem">Add Disk Item</span>
-                </router-link>
-              </li>
-              <!--<li>-->
-                <!--<router-link :to="'/openPhysicalItem'">-->
-                  <!--<span @click="clickOpenPhysicalItem">Add Physical Item</span>-->
-                <!--</router-link>-->
-              <!--</li>-->
-            </ul>
-          </li>
-          <li v-bind:class="{active: activeMenu == 'listItems'}">
-            <router-link :to="'/listItems'">
-              <span @click="clickListItems">
-                <span class="glyphicon glyphicon-th-list"></span> List Items
-              </span>
-            </router-link>
-          </li>
-          <li v-bind:class="{active: activeMenu == 'listTeams'}">
-            <router-link :to="'/listTeams'">
-              <span @click="clickListTeams">
-                <span class="glyphicon glyphicon-th-list"></span> List Teams
-              </span>
-            </router-link>
-          </li>
-        </ul>
+      <template v-if="this.$store.state.isLoggedOn">
+        <template v-if="!this.$store.state.userInfo.userRoles.includes('ADMIN')">
+          <ul class="nav navbar-nav">
+            <li class="dropdown" v-bind:class="{active: openItem}">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <span class="glyphicon glyphicon-plus"></span> Add Item
+                <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <router-link :to="'/openDigitalItem'">
+                    <span @click="clickOpenDigitalItem">Add Disk Item</span>
+                  </router-link>
+                </li>
+                <!--<li>-->
+                  <!--<router-link :to="'/openPhysicalItem'">-->
+                    <!--<span @click="clickOpenPhysicalItem">Add Physical Item</span>-->
+                  <!--</router-link>-->
+                <!--</li>-->
+              </ul>
+            </li>
+            <li v-bind:class="{active: activeMenu == 'listItems'}">
+              <router-link :to="'/listItems'">
+                <span @click="clickListItems">
+                  <span class="glyphicon glyphicon-th-list"></span> List Items
+                </span>
+              </router-link>
+            </li>
+            <li v-bind:class="{active: activeMenu == 'listTeams'}">
+              <router-link :to="'/listTeams'">
+                <span @click="clickListTeams">
+                  <span class="glyphicon glyphicon-th-list"></span> List Teams
+                </span>
+              </router-link>
+            </li>
+          </ul>
+        </template>
         <ul class="nav navbar-nav navbar-right">
           <li>
             <a><span class="glyphicon glyphicon-user"></span> {{userName}}</a>
--- src/main.js
+++ src/main.js
@@ -99,7 +99,6 @@ new Vue({
       store.commit('login')
     },
     logout () {
-      alert('logout')
       store.commit('logout')
     }
   }
